373978267948928f070e5f7e934170cd320c49d9
Larry Baltz <larry@baltz.org>
2022-05-19 19:12:02 +0200

    add function to randomly select a word from the dictionary.

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,8 @@
 defmodule Dictionary do
   def word_list() do
     "assets/words.txt" |> File.read! |> String.trim |> String.split(~r/\n/)
+
+  def random_word do
+    Enum.random(word_list())
   end
 end
