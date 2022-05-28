fb24b0cc032f5139cc1207b64b192560fcbd1598
Larry Baltz <larry@baltz.org>
2022-05-28 20:21:35 +0200

    types for hangman API

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,9 +1,22 @@
 defmodule Hangman do
-  def new_game() do
+  @moduledoc """
+  Game logic for hangman
+  """
 
+  @type game :: any
+  @type state :: :intiaizing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type tally :: %{
+          turns_left: integer,
+          game_state: state,
+          letters: list(String.t()),
+          used: list(String.t())
+        }
+
+  @spec new_game :: game
+  def new_game do
   end
 
-  def guess(game, guess) do
-  
+  @spec make_move(game, String.t()) :: {game, tally}
+  def make_move(_game, _guess) do
   end
 end
