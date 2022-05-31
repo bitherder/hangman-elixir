0a76991526524f9aba0ef49baf8c2035d0b09c36
Larry Baltz <larry@baltz.org>
2022-05-31 18:24:43 +0200

    naive impementation of new Hangman game

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,19 +2,19 @@ defmodule Hangman do
   @moduledoc """
   Game logic for hangman
   """
+  alias Hangman.Impl.Game
 
-  @type game :: any
+  @opaque game :: Game.t
   @type state :: :intiaizing | :won | :lost | :good_guess | :bad_guess | :already_used
   @type tally :: %{
           turns_left: integer,
           game_state: state,
-          letters: list(String.t()),
-          used: list(String.t())
+          letters: list(String.t),
+          used: list(String.t)
         }
 
   @spec new_game :: game
-  def new_game do
-  end
+  defdelegate new_game, to: Game
 
   @spec make_move(game, String.t()) :: {game, tally}
   def make_move(_game, _guess) do
---
+++ hangman/lib/impl/game.ex
@@ -0,0 +1,23 @@
+defmodule Hangman.Impl.Game do
+  @moduledoc """
+  Game logic for Hangman game
+  """
+  @type t :: %__MODULE__{
+    turns_left: integer,
+    game_state: Hangman.state,
+    letters: list(String.t),
+    used: Mapset.t(String.t)
+  }
+  defstruct(
+   turns_left: 7,
+   game_state: :initalizing,
+   letters: [],
+   used: MapSet.new()
+  )
+
+  def new_game do
+    %__MODULE__{
+      letters: Dictionary.random_word |> String.codepoints
+    }
+  end
+end
