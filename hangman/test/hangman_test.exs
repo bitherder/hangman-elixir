8a4befab278260fb0ec8a841a5429f59a4d1b321
Larry Baltz <larry@baltz.org>
2022-06-08 18:25:42 +0200

    test some game behavior

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -4,7 +4,23 @@ defmodule HangmanTest do
   """
 
   use ExUnit.Case
+  alias Hangman.Impl.Game
+
   doctest Hangman
+
+    test "new game returns structure" do
+    game = Game.new_game()
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "new game returns correct word" do
+    game = Game.new_game("wombat")
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
 
   test "all letters are lower case" do
     game = Hangman.new_game()
