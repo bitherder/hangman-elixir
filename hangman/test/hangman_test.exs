65dbf584bc0d6a6a61cb0e336276dd094f7c54c7
Larry Baltz <larry@baltz.org>
2022-06-08 12:45:59 +0200

    test words are lower case

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -1,8 +1,19 @@
 defmodule HangmanTest do
+  @moduledoc """
+  Test hangman interface behavior
+  """
+
   use ExUnit.Case
   doctest Hangman
 
-  test "greets the world" do
-    assert Hangman.hello() == :world
+  test "all letters are lower case" do
+    game = Hangman.new_game()
+    assert game.letters |> Enum.map(&lower?/1) |> Enum.reduce(&(&1 and &2))
   end
+
+  defp lower?(string) do
+    String.match?(string, ~r/^[[:alnum:]]$/)
+  end
+
+  defp all_true?(bool_list) do
 end
