96d26f048ab104999d970e712eb341f8ab38e62c
Larry Baltz <larry@baltz.org>
2022-06-08 19:15:13 +0200

    Add missing module doc

--- hangman/lib/type.ex
+++ hangman/lib/type.ex
@@ -1,4 +1,8 @@
 defmodule Hangman.Type do
+  @moduledoc """
+  Common types for hangman game
+  """
+
   @type state :: :initiaizing | :won | :lost | :good_guess | :bad_guess | :already_used
 
   @type tally :: %{
